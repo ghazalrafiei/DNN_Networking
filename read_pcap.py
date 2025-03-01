aeda990dc7acf3bd851a13f9754af2e53a19ea72
ghazalrafiei <gh.rafiei99@gmail.com>
2025-02-28 16:49:28 -0800

    Read Pcap Files

---
+++ read_pcap.py
@@ -0,0 +1,23 @@
+import pyshark
+
+PCAP_FILE = './20210115-000005-00459768.mia.pcap'
+READ_LINES = 100
+
+if __name__ == '__main__':
+    cap = pyshark.FileCapture(PCAP_FILE)
+
+    i = 0
+    for pkt in cap:
+        i+=1
+        if i > READ_LINES:
+            break
+        if 'IP' in pkt:
+            print('source:',pkt.ip.src,'destination:', pkt.ip.dst)
+
+    # print(cap[0])
+    # print(cap[1])
+    # print(cap[2])
+    # print(cap[3])
+    # print(cap[4])
+    # print(cap[5])
+    # print(cap[6])
